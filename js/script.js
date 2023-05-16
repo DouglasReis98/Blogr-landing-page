26c7859ac587e5dd661dc575a108006ac0cfde6a
DouglasReis <douglasamaralreis@outlook.com>
2023-05-15 22:52:24 -0300

    add js

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ const nav = document.querySelector("nav");
 
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle("active");
+    if(nav.classList.contains("active")){
+        btnMenu.classList.remove("fa-bars");
+        btnMenu.classList.add("fa-x");
+    }else{
+        btnMenu.classList.remove("fa-x");
+        btnMenu.classList.add("fa-bars");
+    }
 })
