dc1d09c0d14eac7d261562456439bacb44c4a7d7
DouglasReis <douglasamaralreis@outlook.com>
2023-05-19 19:05:31 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const btnMenu = document.getElementById("menu-hamburger");
 const nav = document.querySelector("nav");
+const menu_li = document.querySelectorAll("#menu > li");
+const menuSpan = document.querySelectorAll("#menu li span i");
+const dropdown = document.querySelectorAll(".dropdown");
 
 btnMenu.addEventListener('click', () => {
     nav.classList.toggle("active");
@@ -10,4 +13,23 @@ btnMenu.addEventListener('click', () => {
         btnMenu.classList.remove("fa-x");
         btnMenu.classList.add("fa-bars");
     }
-})
+});
+
+function menu(){
+for (let i = 0; i < menu_li.length; i++) {
+
+menu_li[i].addEventListener('mouseover', () => {
+    dropdown[i].style.display = "block";
+    menuSpan[i].style.transform = "rotate(180deg)";
+});
+
+menu_li[i].addEventListener('mouseout', () => {
+    dropdown[i].style.display = "none";
+    menuSpan[i].style.transform = "rotate(0deg)";
+});
+    
+}
+
+}
+
+menu();
